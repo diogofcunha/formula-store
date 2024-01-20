e57d00c14a981c0fd9e0c5cc93ae2a85e4fbee3f
Diogo Cunha <diogofncunha@gmail.com>
2024-01-20 22:16:38 +0000

    test: 💍 added test coverage for calculate on field add

--- src/__tests__/addField.test.ts
+++ src/__tests__/addField.test.ts
@@ -1,51 +1,99 @@
 import { createFormulaStore } from "../";
 
-test("addField should fail if a listed dependency doesn`t exist", () => {
-  const store = createFormulaStore();
-  expect(() =>
-    store.addField({
-      dependencies: ["a-b-c"],
-      id: "dfc",
-      value: 1,
-      calculate: () => 1
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `"Could not find dependencies a-b-c for field dfc"`
-  );
-});
-
-test("addField should fail if multiple listed dependency doesn`t exist", () => {
-  const store = createFormulaStore();
-  expect(() =>
-    store.addField({
-      dependencies: ["a-b-c", "d-e-f"],
-      id: "dfc",
-      value: 1,
-      calculate: () => 1
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `"Could not find dependencies a-b-c, d-e-f for field dfc"`
-  );
-});
-
-test("addField should fail if trying to re-add field", () => {
-  const store = createFormulaStore();
-
-  store.addField({
-    dependencies: [],
-    id: "dfc",
-    value: 1,
-    calculate: () => 1
+describe("addField", () => {
+  test("should fail if a listed dependency doesn`t exist", () => {
+    const store = createFormulaStore();
+    expect(() =>
+      store.addField({
+        dependencies: ["a-b-c"],
+        id: "dfc",
+        value: 1,
+        calculate: () => 1
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `"Could not find dependencies a-b-c for field dfc"`
+    );
   });
 
-  expect(() =>
+  test("should fail if multiple listed dependency doesn`t exist", () => {
+    const store = createFormulaStore();
+    expect(() =>
+      store.addField({
+        dependencies: ["a-b-c", "d-e-f"],
+        id: "dfc",
+        value: 1,
+        calculate: () => 1
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `"Could not find dependencies a-b-c, d-e-f for field dfc"`
+    );
+  });
+
+  test("should fail if trying to re-add field", () => {
+    const store = createFormulaStore();
+
     store.addField({
       dependencies: [],
       id: "dfc",
       value: 1,
       calculate: () => 1
-    })
-  ).toThrowErrorMatchingInlineSnapshot(
-    `"Formula field "dfc" already exists, please delete the field if you want to update it."`
-  );
+    });
+
+    expect(() =>
+      store.addField({
+        dependencies: [],
+        id: "dfc",
+        value: 1,
+        calculate: () => 1
+      })
+    ).toThrowErrorMatchingInlineSnapshot(
+      `"Formula field "dfc" already exists, please delete the field if you want to update it."`
+    );
+  });
+
+  test("should calculate correctly when adding field with dependencies", () => {
+    const store = createFormulaStore();
+
+    // Add fields for dependencies.
+    store.addField({
+      dependencies: [],
+      id: "a",
+      value: 1,
+      calculate: () => 1
+    });
+
+    store.addField({
+      dependencies: [],
+      id: "b",
+      value: 3,
+      calculate: () => 3
+    });
+
+    const calculateSpy = jest.fn();
+
+    store.addField({
+      dependencies: ["a", "b"],
+      id: "c",
+      value: 0,
+      calculate: calculateSpy
+    });
+
+    expect(calculateSpy).toHaveBeenCalledTimes(1);
+    expect(calculateSpy).toHaveBeenCalledWith([1, 3]);
+  });
+
+  test("shouldn't calculate when adding field without dependencies", () => {
+    const store = createFormulaStore();
+
+    const calculateSpy = jest.fn();
+
+    store.addField({
+      dependencies: [],
+      id: "c",
+      value: 0,
+      calculate: calculateSpy
+    });
+
+    expect(calculateSpy).toHaveBeenCalledTimes(0);
+  });
 });
