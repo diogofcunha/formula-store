d7ea42fe95555ec71becae823cc29803011c2bd0
Diogo Cunha <diogofncunha@gmail.com>
2024-01-20 21:39:58 +0000

    feat: 🎸 created addField on formula store

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-export default function add(a: number, b: number) {
-  return a + b;
+import {
+  FormulaFieldDependencyError,
+  FormulaFieldDuplicatedError
+} from "./errors";
+import { FormulaStore } from "./types";
+import { Graph, Node } from "fast-graph";
+
+export function createFormulaStore(): FormulaStore {
+  const addedFields = new Map<string, Node<unknown>>();
+  const fieldGraph = new Graph();
+
+  return {
+    addField: ({ id, value, dependencies }) => {
+      if (addedFields.has(id)) {
+        throw new FormulaFieldDuplicatedError(id);
+      }
+
+      const missingFields = dependencies.filter(d => !addedFields.has(d));
+
+      if (missingFields.length > 0) {
+        throw new FormulaFieldDependencyError(id, missingFields);
+      }
+
+      const node = new Node(id, value);
+      fieldGraph.addNode(node);
+
+      for (const d of dependencies) {
+        const parentField = addedFields.get(d) as Node<unknown>;
+        fieldGraph.addEdge(parentField, node);
+        addedFields.set(id, node);
+      }
+    }
+  };
 }
