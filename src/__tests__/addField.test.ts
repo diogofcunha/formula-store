ca58971ab130ef79f984d3eefe0f2c82aac70569
Diogo Cunha <diogofncunha@gmail.com>
2024-01-20 21:48:53 +0000

    test: 💍 added addField failure tests

---
+++ src/__tests__/addField.test.ts
@@ -0,0 +1,47 @@
+import { createFormulaStore } from "../";
+
+test("addField should fail if a listed dependency doesn`t exist", () => {
+  const store = createFormulaStore();
+  expect(() =>
+    store.addField({
+      dependencies: ["a-b-c"],
+      id: "dfc",
+      value: 1
+    })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Could not find dependencies a-b-c for field dfc"`
+  );
+});
+
+test("addField should fail if multiple listed dependency doesn`t exist", () => {
+  const store = createFormulaStore();
+  expect(() =>
+    store.addField({
+      dependencies: ["a-b-c", "d-e-f"],
+      id: "dfc",
+      value: 1
+    })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Could not find dependencies a-b-c, d-e-f for field dfc"`
+  );
+});
+
+test("addField should fail if trying to re-add field", () => {
+  const store = createFormulaStore();
+
+  store.addField({
+    dependencies: [],
+    id: "dfc",
+    value: 1
+  });
+
+  expect(() =>
+    store.addField({
+      dependencies: [],
+      id: "dfc",
+      value: 1
+    })
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Formula field "dfc" already exists, please delete the field if you want to update it."`
+  );
+});
